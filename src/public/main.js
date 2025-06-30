def72c505fd835332c18bb3b2b82caf8531ae4bb
Eric Skaftason <skaftasoneric@gmail.com>
2025-06-30 00:31:29 -0400

    set dimensions to fit all screen sizes

--- src/public/main.js
+++ src/public/main.js
@@ -8,7 +8,25 @@ const game_container = document.querySelector('.game');
 const board_ui = document.querySelector('.board_ui');
 const external_ui = document.querySelector('.external_ui');
 
-const width = 600;
-const height = 600;
+
+// Set dimensions
+
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
+
+const bodyStyle = getComputedStyle(document.body);
+
+const horizontalMargin = parseFloat(bodyStyle.marginLeft) + parseFloat(bodyStyle.marginRight);
+const verticalMargin = parseFloat(bodyStyle.marginTop) + parseFloat(bodyStyle.marginBottom);
+
+const maxAvailableWidth = screenWidth - horizontalMargin;
+const maxAvailableHeight = screenHeight - verticalMargin;
+
+const minDimension = Math.min(maxAvailableWidth, maxAvailableHeight, 600);
+
+const width = minDimension;
+const height = minDimension;
+
+// Define game
 
 const game = new Game(game_container, canvases, board_ui, external_ui, width, height);
